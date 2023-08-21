74beb80d15757e1e8f74500e94090f03367d789d
Vardhan Patil <demonslayervardhan@gmail.com>
2023-08-21 19:39:52 +0530

    Update database schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,24 @@ CREATE TABLE
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
     );
+
+CREATE TABLE
+    IF NOT EXISTS expenses (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        description TEXT NOT NULL,
+        expense_time TIMESTAMP NOT NULL DEFAULT now(),
+        user_id UUID REFERENCES users (id) ON DELETE CASCADE NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS tags (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        name TEXT NOT NULL,
+        user_id UUID REFERENCES users (id) ON DELETE CASCADE NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS expenses_tags (
+        tag UUID REFERENCES tags (id) ON DELETE CASCADE NOT NULL,
+        expense_id UUID REFERENCES expenses (id) ON DELETE CASCADE NOT NULL
+    );
